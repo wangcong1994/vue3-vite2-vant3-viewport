fb6c9fbeb2cbfd2c7cfab4bafdbd800ff32e94b7
wangcong1994 <779033969@qq.com>
2021-03-26 16:37:40 +0800

    移动端viewport适配

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <van-image :src="image" class="image"></van-image>
+</template>
+
+<script setup>
+import image from "../assets/logo.png?url";
+</script>
+
+<style>
+.image {
+  font-size: 14px;
+  width: 100px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Vant from "vant";
+import "vant/lib/index.css";
+createApp(App).use(Vant).mount("#app");
